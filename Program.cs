a1ab317b08b33fd9008d2fa961026a43659a71be
Ricardo Vianna <viannarp@gmail.com>
2024-10-13 22:07:41 -0300

    Entrega projeto Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//Criando uma instância de Nokia
+Console.WriteLine("Testando o Nokia:");
+Nokia meuNokia = new Nokia("123456789", "Nokia 3310", "111111111111111", 64);
+meuNokia.Ligar();
+meuNokia.ReceberLigacao();
+meuNokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("\n");
+
+//Criando uma instância de iPhone
+Console.WriteLine("Testando o iPhone:");
+Iphone meuIphone = new Iphone("987654321", "iPhone 12", "777777777777777", 128);
+meuIphone.Ligar();
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("Facebook");
+
+
